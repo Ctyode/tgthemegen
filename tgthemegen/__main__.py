ddbdb30310ea7c7744f63a7316d1f1f649ee1b9d
Nicktael Rawieo <b.d.demin@gmail.com>
2017-01-16 02:15:11 +0400

    I've done something (. _. )

--- tgthemegen/__main__.py
+++ tgthemegen/__main__.py
@@ -1,23 +1,24 @@
 import os
+import zipfile
 from argparse import ArgumentParser
 from tgthemegen.generate import generate, Color
-import zipfile
 
 # люблю всяких котиков
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--primary-color', required=True)
-    parser.add_argument('--accent-color', required=True)
-    parser.add_argument('--background-color', required=True)
-    parser.add_argument('--tiled', required=True)
-    parser.add_argument('--background', required=True)
+    parser.add_argument('--color-primary', required=True)
+    parser.add_argument('--color-accent', required=True)
+    parser.add_argument('--color-background', required=True)
+    parser.add_argument('--tiled')
+    parser.add_argument('--background')
     args = parser.parse_args()
+    img_source = args.tiled or args.background
     here = os.path.dirname(os.path.abspath(__file__))
     with zipfile.ZipFile('name.tdesktop-theme', 'w') as myzip:
-        myzip.write(os.path.join(os.getcwd(), args.background), arcname='background.jpg')
+        myzip.write(img_source, arcname=(('tiled' if args.tiled else 'background') + os.path.splitext(img_source)[1]))
         myzip.writestr('colors.tdesktop-theme',
                        '\n'.join(list(map(lambda x: '{}: {};'.format(x[0], x[1]),
-                                          generate(primary=Color.parse(args.primary_color),
-                                                   accent=Color.parse(args.accent_color),
-                                                   background=Color.parse(args.background_color).items())))))
+                                          generate(primary=Color.parse(args.color_primary),
+                                                   accent=Color.parse(args.color_accent),
+                                                   background=Color.parse(args.color_background)).items()))))
