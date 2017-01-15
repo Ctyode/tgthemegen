d51877dcca5235f64d0cf89b77ff66f97f56259b
Nicktael Rawieo <b.d.demin@gmail.com>
2017-01-16 02:17:30 +0400

    Added --out option

--- tgthemegen/__main__.py
+++ tgthemegen/__main__.py
@@ -12,10 +12,11 @@ if __name__ == '__main__':
     parser.add_argument('--color-background', required=True)
     parser.add_argument('--tiled')
     parser.add_argument('--background')
+    parser.add_argument('--out', required=True)
     args = parser.parse_args()
     img_source = args.tiled or args.background
     here = os.path.dirname(os.path.abspath(__file__))
-    with zipfile.ZipFile('name.tdesktop-theme', 'w') as myzip:
+    with zipfile.ZipFile('{}.tdesktop-theme'.format(args.out), 'w') as myzip:
         myzip.write(img_source, arcname=(('tiled' if args.tiled else 'background') + os.path.splitext(img_source)[1]))
         myzip.writestr('colors.tdesktop-theme',
                        '\n'.join(list(map(lambda x: '{}: {};'.format(x[0], x[1]),
